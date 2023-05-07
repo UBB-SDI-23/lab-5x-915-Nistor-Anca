976227125adf2abe510394582a98dae5f9fee495
915-Nistor-Anca <ancanistor16@gmail.com>
2023-05-07 15:25:12 +0300

    Update AnimalsGetAll.tsx

--- lab5/src/components/Animals/AnimalsGetAll.tsx
+++ lab5/src/components/Animals/AnimalsGetAll.tsx
@@ -35,7 +35,7 @@ export const AllAnimals = () => {
 			.then((data) => {
 				setAnimals(data);
 				setLoading(false);
-				console.log(data);
+				
 			});
 	}, []);
 
@@ -54,16 +54,72 @@ export const AllAnimals = () => {
 
 			{loading && <CircularProgress />}
 			{!loading && animals.length === 0 && <p>No animals found</p>}
-			{!loading && (
+			{!loading && animals.length > 0  &&(
 				<Toolbar>
-					
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/animals/add`}>
+						<Tooltip title="Add a new animal" arrow>
+							<AddIcon color="primary" />
+						</Tooltip>
+					</IconButton>
 					<Button
 						onClick={orderByKilograms}
 					>Order By Kilograms
 					</Button>
 				</Toolbar>
 			)}
-			
+			{!loading && animals.length > 0 && (
+				<TableContainer component={Paper}>
+					<Table sx={{ minWidth: 650 }} aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell>#</TableCell>
+								<TableCell align="right">Name</TableCell>
+								<TableCell align="right">Gender</TableCell>
+								<TableCell align="right">Birth date</TableCell>
+								<TableCell align="right">Kilograms</TableCell>
+								<TableCell align="right">Favourite toy</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{animals.map((animal, index) => (
+								<TableRow key={animal.id}>
+									<TableCell component="th" scope="row">
+										{index + 1}
+									</TableCell>
+									<TableCell component="th" scope="row">
+										<Link to={`/animals/${animal.id}/details`} title="View animal details">
+											{animal.name}
+										</Link>
+									</TableCell>
+									<TableCell align="right">{animal.name}</TableCell>
+									<TableCell align="right">{animal.gender}</TableCell>
+									<TableCell align="right">{animal.birth_date.toString()}</TableCell>
+									<TableCell align="right">{animal.kilograms}</TableCell>
+									<TableCell align="right">{animal.favourite_toy}</TableCell>
+									<TableCell align="right">
+										<IconButton
+											component={Link}
+											sx={{ mr: 3 }}
+											to={`/animals/${animal.id}/details`}>
+											<Tooltip title="View animal details" arrow>
+												<ReadMoreIcon color="primary" />
+											</Tooltip>
+										</IconButton>
+
+										<IconButton component={Link} sx={{ mr: 3 }} to={`/animals/${animal.id}/edit`}>
+											<EditIcon />
+										</IconButton>
+
+										<IconButton component={Link} sx={{ mr: 3 }} to={`/animals/${animal.id}/delete`}>
+											<DeleteForeverIcon sx={{ color: "red" }} />
+										</IconButton>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			)}
 		</Container>
 	);
 };
