ff31ea1ab2b7b0bba3d5cd6fcfe8e2de29fcd7a1
915-Nistor-Anca <ancanistor16@gmail.com>
2023-05-08 12:17:40 +0300

    updates

--- lab5/src/constants.ts
+++ lab5/src/constants.ts
@@ -1,5 +1,5 @@
 export const PROD_BACKEND_API_URL = "/api";
 //export const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
-export const DEV_BACKEND_API_URL = "http://ec2-13-50-233-252.eu-north-1.compute.amazonaws.com";
+export const DEV_BACKEND_API_URL = "http://ec2-16-16-210-41.eu-north-1.compute.amazonaws.com";
 export const BACKEND_API_URL =
   process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
